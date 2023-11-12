cb55ef5e318514831075781128f3f982c126e981
sakurakotubaki <gitonly543@gmail.com>
2023-11-13 00:26:33 +0900

    feat: auth

--- FirebaseAuthTutorial/FirebaseAuthTutorialApp.swift
+++ FirebaseAuthTutorial/FirebaseAuthTutorialApp.swift
@@ -1,17 +1,29 @@
-//
-//  FirebaseAuthTutorialApp.swift
-//  FirebaseAuthTutorial
-//
-//  Created by 橋本純一 on 2023/11/12.
-//
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct FirebaseAuthTutorialApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+            @StateObject var viewModel = AuthViewModel()
+
+            var body: some Scene {
+                WindowGroup {
+                    // ログイン状態によって画面遷移するページを変更する
+                    if viewModel.isAuthenticated {
+                        HelloPage(viewModel: viewModel)
+                    } else {
+                        SignInView(viewModel: viewModel)
+                    }
+                }
+            }
 }
