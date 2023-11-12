372934a0352aac9312dc83158498e2aabf6862ae
sakurakotubaki <gitonly543@gmail.com>
2023-11-12 23:46:05 +0900

    Initial Commit

---
+++ FirebaseAuthTutorial/FirebaseAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthTutorialApp.swift
+//  FirebaseAuthTutorial
+//
+//  Created by 橋本純一 on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
